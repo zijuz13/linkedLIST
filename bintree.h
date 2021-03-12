59d34dbd82591fe0e95b0220eda6133a1c60156a
Zijun Zhou <70651186+zijuz13@users.noreply.github.com>
2021-03-12 11:56:31 +0800

    Update bintree.h

--- bintree.h
+++ bintree.h
@@ -41,7 +41,6 @@ void bintree_print_in_order(BinaryTree*);
 void bintree_print_pre_order(BinaryTree*);
 void bintree_print_post_order(BinaryTree*);
 void bintree_print_reverse_order(BinaryTree*);
-bool _bintree_insert_recursive(BinaryTree*, BinaryTreeNode*, void*);
 bool _bintree_search_recursive(BinaryTree*, BinaryTreeNode*, void*);
 void _bintree_in_order_recursive(BinaryTree*, BinaryTreeNode*);
 void _bintree_pre_order_recursive(BinaryTree*, BinaryTreeNode*);
