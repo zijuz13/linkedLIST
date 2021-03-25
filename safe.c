1273436849c6b1035b3f23a1725fac8c9c8d1924
Zijun Zhou <70651186+zijuz13@users.noreply.github.com>
2021-03-25 13:07:38 +0800

    Create safe.c

---
+++ safe.c
@@ -0,0 +1,25 @@
+#include "safe.h"
+sem_t flag;
+void* counting(void* count){
+if(count==NULL)return NULL;
+sem_wait(&flag);
+Counter* counter=(Counter*)count;
+if(counter->counter<1000){
+counter->counter++;
+printf("%d\n",counter->counter);
+}
+sem_post(&flag);
+return NULL;
+}
+void count_variable(int n){
+    sem_init(&flag,0,1);
+    Counter counter;
+    counter.counter=0;
+    pthread_t* thread1=malloc(n*sizeof(pthread_t));
+    for(int i=0;i<n;++i){
+    pthread_create((thread1+i),NULL,counting,&counter);
+    }
+    for(int i=0;i<n;++i){
+        pthread_join(*(thread1+i),NULL);
+    }
+}
