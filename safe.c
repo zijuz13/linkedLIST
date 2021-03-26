458b813375793ef739d0f6b4ef97917ef33b4274
Zijun Zhou <70651186+zijuz13@users.noreply.github.com>
2021-03-26 10:20:35 +0800

    Update safe.c

--- safe.c
+++ safe.c
@@ -13,11 +13,11 @@ return NULL;
 }
 void count_variable(int n){
     sem_init(&flag,0,1);
-    Counter counter;
-    counter.counter=0;
+    Counter* counter=malloc(sizeof(Counter));
+    counter->counter=0;
     pthread_t* thread1=malloc(n*sizeof(pthread_t));
     for(int i=0;i<n;++i){
-    pthread_create((thread1+i),NULL,counting,&counter);
+    pthread_create((thread1+i),NULL,counting,counter);
     }
     for(int i=0;i<n;++i){
         pthread_join(*(thread1+i),NULL);
